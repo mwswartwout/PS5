6cd087a5466b9456e9a69bee216f7737cfce865d
xpharry <peng.xu4@case.edu>
2016-02-28 17:27:53 -0500

    para change for real test

--- packages/alpha_mobot_pub_des_state/src/pub_des_state_path_replanning_client.cpp
+++ packages/alpha_mobot_pub_des_state/src/pub_des_state_path_replanning_client.cpp
@@ -36,7 +36,7 @@ int main(int argc, char **argv) {
     geometry_msgs::PoseStamped pose_stamped;
     pose_stamped.header.frame_id = "world";
     geometry_msgs::Pose pose;
-    pose.position.x = -5.0; // say desired x-coord is 5
+    pose.position.x = -1.5; // say desired x-coord is 5
     pose.position.y = 0.0;
     pose.position.z = 0.0; // let's hope so!
     quat = convertPlanarPhi2Quaternion(0);
@@ -44,7 +44,7 @@ int main(int argc, char **argv) {
     pose_stamped.pose = pose;
     path_srv.request.path.poses.push_back(pose_stamped);
  
-    pose.position.y = -5.0;
+    pose.position.y = -1.5;
     pose_stamped.pose = pose;
     path_srv.request.path.poses.push_back(pose_stamped);
 
