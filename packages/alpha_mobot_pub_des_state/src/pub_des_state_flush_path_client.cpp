7ffbba44d4c4e030a2c02b6fc95f65e99de2dc85
xpharry <peng.xu4@case.edu>
2016-02-28 16:38:25 -0500

    tested path replanning

--- packages/alpha_mobot_pub_des_state/src/pub_des_state_flush_path_client.cpp
+++ packages/alpha_mobot_pub_des_state/src/pub_des_state_flush_path_client.cpp
@@ -3,23 +3,23 @@
 
 #include <ros/ros.h>
 #include <iostream>
-#include <alpha_mobot_pub_des_state/path.h>
+#include <std_srvs/Trigger.h>
 
 using namespace std;
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "flush_path_client");
     ros::NodeHandle n;
-    ros::ServiceClient client = n.serviceClient<alpha_mobot_pub_des_state::path>("flush_path_queue_service");
+    ros::ServiceClient client = n.serviceClient<std_srvs::Trigger>("flush_path_queue_service");
     
     while (!client.exists()) {
       ROS_INFO("waiting for service...");
       ros::Duration(1.0).sleep();
     }
     ROS_INFO("connected client to service");
-    alpha_mobot_pub_des_state::path path_srv;
+    std_srvs::Trigger trigger;
 
-    client.call(path_srv); // Does it have to put a parameter? here actually we are calling a function without using parameters
+    client.call(trigger); // Does it have to put a parameter? here actually we are calling a function without using parameters
 
     return 0;
 }
